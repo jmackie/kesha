6dd2c631e7d8f305db99f0949403db69901e5611
Jordan Mackie <jmackie@protonmail.com>
2019-08-04 19:49:03 +0100

    Replicate Nix base32 encoding
    Dunno what I'm doing really, just ported from the Nix source...

--- src/Kesha.hs
+++ src/Kesha.hs
@@ -1,21 +1,103 @@
+-- https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/fetchgit/nix-prefetch-git
+{-# LANGUAGE OverloadedStrings #-}
 module Kesha
   ( hash
+
+  , hashWith
+  , Opts(..)
+  , HashAlgo(..)
+  , HashRepr(..)
   ) where
 
-import Prelude
+import Prelude hiding ((!!))
 
 import qualified Crypto.Hash.SHA256 as SHA256
 import qualified Data.ByteString as BS
-import qualified Data.ByteString.Base32 as Base32
-import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString.Char8 as ASCII
+import qualified Data.Char as Char
+import qualified Data.Hex as Hex
+import qualified Data.Sequence as Seq
+
+import Data.Bits ((.&.), (.|.), shiftL, shiftR)
+import Data.Maybe (fromJust)
+import Data.Word (Word8)
 
 import qualified Kesha.NAR as NAR
 
 hash :: FilePath -> IO (Either String BS.ByteString)
-hash path = fmap (sha256sum . NAR.dump) <$> NAR.localPack path
+hash = hashWith (Opts SHA256 Base32)
 
-sha256sum :: BSL.ByteString -> BS.ByteString
-sha256sum = Base32.encode . SHA256.hashlazy
+data Opts
+  = Opts
+      { hashAlgo :: HashAlgo
+      , hashRepr :: HashRepr
+      }
 
--- https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/fetchgit/nix-prefetch-git
+data HashAlgo
+  = SHA256
+
+-- |
+-- https://github.com/NixOS/nix/blob/master/src/libutil/hash.hh
+hashSizeForAlgo :: HashAlgo -> Int
+hashSizeForAlgo SHA256 = 32
+
+data HashRepr
+  = Hex
+  | Base32
+
+hashWith :: Opts -> FilePath -> IO (Either String BS.ByteString)
+hashWith opts path =
+  fmap (printNar (hashAlgo opts) (hashRepr opts)) <$> NAR.localPack path
+
+printNar :: HashAlgo -> HashRepr -> NAR.NAR -> BS.ByteString
+printNar SHA256 Hex
+  = ASCII.map Char.toLower
+  . Hex.hex
+  . SHA256.hashlazy
+  . NAR.dump
+
+printNar SHA256 Base32
+  = ASCII.map Char.toLower
+  . printHash32 SHA256
+  . SHA256.hashlazy
+  . NAR.dump
+
+-- |
+-- This is quite nasty...
+--
 -- https://github.com/NixOS/nix/blob/master/src/libutil/hash.cc
+printHash32 :: HashAlgo -> BS.ByteString -> BS.ByteString
+printHash32 algo rawHash = go (len - 1) ""
+  where
+    hashSize :: Int
+    hashSize = hashSizeForAlgo algo
+
+    len :: Int
+    len = (hashSize * 8 - 1) `div` 5 + 1
+
+    go :: Int -> BS.ByteString -> BS.ByteString
+    go n accum
+      | n < 0 = accum
+      | otherwise =
+          go (pred n) $
+            ASCII.snoc accum (base32Chars !! (fromIntegral c .&. 0x1f))
+        where
+          b, i, j :: Int
+          b = n * 5
+          i = b `div` 8
+          j = b `mod` 8
+
+          c :: Word8
+          c = ((bytes !! i) `shiftR` j) .|.
+              (if i >= (hashSize - 1) then 0 else (bytes !! (i + 1)) `shiftL` (8 - j))
+
+    bytes :: Seq.Seq Word8
+    bytes = Seq.fromList (BS.unpack rawHash)
+
+    (!!) :: Seq.Seq a -> Int -> a
+    (!!) xs i = fromJust (Seq.lookup i xs)
+    infixl 9 !!
+
+-- omitted: E O U T
+base32Chars :: Seq.Seq Char
+base32Chars = Seq.fromList "0123456789abcdfghijklmnpqrsvwxyz"
