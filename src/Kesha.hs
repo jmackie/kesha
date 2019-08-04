abe9673b6486c1860e7882ff34271839933a1bbf
Jordan Mackie <jmackie@protonmail.com>
2019-08-04 16:27:05 +0100

    Use base32 for consistency with nix-prefetch-git

--- src/Kesha.hs
+++ src/Kesha.hs
@@ -6,7 +6,7 @@ import Prelude
 
 import qualified Crypto.Hash.SHA256 as SHA256
 import qualified Data.ByteString as BS
-import qualified Data.ByteString.Base16 as Base16
+import qualified Data.ByteString.Base32 as Base32
 import qualified Data.ByteString.Lazy as BSL
 
 import qualified Kesha.NAR as NAR
@@ -15,4 +15,4 @@ hash :: FilePath -> IO (Either String BS.ByteString)
 hash path = fmap (sha256sum . NAR.dump) <$> NAR.localPack path
 
 sha256sum :: BSL.ByteString -> BS.ByteString
-sha256sum = Base16.encode . SHA256.hashlazy
+sha256sum = Base32.encode . SHA256.hashlazy
